7b259324a02029b2160556d3628964b8fdf9cb6f
JumbooWong <493182574@qq.com>
2019-01-03 20:02:41 +0800

    更新

--- blog1/comments.py
+++ blog1/comments.py
@@ -33,8 +33,9 @@ def commentPage(request):
             request.session['page_num_list'] = showPageList(request.session['page_num'],request.session['show_num'],current=request.session['current_num'],type='pre')
 
     else:
-        # current_num = int(re.findall('\d',url)[0])
-        current_num = request.GET.get('page')
+        current_num = int(re.findall('\d',url)[0])
+        # current_num = request.GET.get('page')
+        print(current_num)
         request.session['current_num'] = current_num
 
     return redirect(pre_url)
@@ -44,9 +45,9 @@ from blog1.dao.messages import deleteMessage,messageCount
 #删除评论
 def commentDel(request):
     url = request.path
-    # id,floor = int(re.findall('\d+',url)[0]),int(re.findall('\d+',url)[1])
-    id = request.GET.get('id')
-    floor = request.GET.get('floor')
+    id,floor = int(re.findall('\d+',url)[0]),int(re.findall('\d+',url)[1])
+    # id = request.GET.get('id')
+    # floor = request.GET.get('floor')
     show_num = request.session.get('show_num')
     message_count = messageCount()
     del_status = deleteMessage(id)
@@ -55,7 +56,6 @@ def commentDel(request):
             request.session['current_num'] -= 1
             print('删除后的页码',request.session['current_num'])
         request.session['change'] = 'del'
-
         print('删除成功！')
     else:print('删除失败！')
     pre_url = request.session.get('url')
